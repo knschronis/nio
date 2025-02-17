524d23e8bd0b1148e90d8f47424174228e3a7a8f
knschronis <kns.chronis@gmail.com>
2025-02-17 15:48:37 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,226 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-display-window",
+      "name": "Display Window",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6492333770577456,
+          "pitch": 0.4560736176936544,
+          "rotation": 0,
+          "target": "1-casual-project"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-casual-project",
+      "name": "Casual Project",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.26442819541178686,
+          "pitch": 0.295691223935977,
+          "rotation": 0,
+          "target": "2-shoes-project"
+        },
+        {
+          "yaw": -2.8868737097538393,
+          "pitch": 0.7551049705587793,
+          "rotation": 0,
+          "target": "0-display-window"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-shoes-project",
+      "name": "Shoes Project",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8319101016124026,
+          "pitch": 0.5121460955724455,
+          "rotation": 0.7853981633974483,
+          "target": "1-casual-project"
+        },
+        {
+          "yaw": 0.5456566606401925,
+          "pitch": 0.31856386585503316,
+          "rotation": 0,
+          "target": "3-fashion-project"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-fashion-project",
+      "name": "Fashion Project",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.12333263073241696,
+        "pitch": -0.1324857812969924,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.975546472913848,
+          "pitch": 0.3766313054546444,
+          "rotation": 0,
+          "target": "2-shoes-project"
+        },
+        {
+          "yaw": 0.3112023858471762,
+          "pitch": 0.2870960459605101,
+          "rotation": 0,
+          "target": "4-tailoring-project"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-tailoring-project",
+      "name": "Tailoring Project",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1769543298360361,
+          "pitch": 0.3888869567930229,
+          "rotation": 0,
+          "target": "3-fashion-project"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.94766299121755,
+          "pitch": 0.05964914741384142,
+          "title": "&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;No Name Stores",
+          "text": "<p class=\"MsoNormal\" align=\"center\" style=\"text-align:center\"><font face=\"Segoe UI Historic, sans-serif\"><span style=\"font-size: 13.3333px;\"><b>Monastiraki&nbsp;</b></span></font></p><p class=\"MsoNormal\" align=\"center\" style=\"text-align:center\"><font face=\"Segoe UI Historic, sans-serif\"><span style=\"font-size: 13.3333px;\"><b>(8 Athinas &amp; Agias Irinis str.)</b></span></font></p><p class=\"MsoNormal\" align=\"center\" style=\"text-align:center\"><font face=\"Segoe UI Historic, sans-serif\"><span style=\"font-size: 13.3333px;\"><b><br></b></span></font></p><p class=\"MsoNormal\" align=\"center\" style=\"text-align:center\"><font face=\"Segoe UI Historic, sans-serif\"><span style=\"font-size: 13.3333px;\"><b>Nea Ionia (236 Irakleiou av.)</b></span></font></p><p class=\"MsoNormal\" align=\"center\" style=\"text-align:center\"><font face=\"Segoe UI Historic, sans-serif\"><span style=\"font-size: 13.3333px;\"><b>Peristeri (3 Rousvelt str.)</b></span></font></p><p class=\"MsoNormal\" align=\"center\" style=\"text-align:center\"><font face=\"Segoe UI Historic, sans-serif\"><span style=\"font-size: 13.3333px;\"><b>Kifisia (13-15 Kiriazi str.)</b></span></font></p><p class=\"MsoNormal\" align=\"center\" style=\"text-align:center\"><font face=\"Segoe UI Historic, sans-serif\"><span style=\"font-size: 13.3333px;\"><b>Glyfada (39 Aggelou Metaxa str)</b></span></font></p><p class=\"MsoNormal\" align=\"center\" style=\"text-align:center\"><font face=\"Segoe UI Historic, sans-serif\"><span style=\"font-size: 13.3333px;\"><b>Piraeus (159 Androutsou str.)</b></span></font></p><p class=\"MsoNormal\" align=\"center\" style=\"text-align:center\"><font face=\"Segoe UI Historic, sans-serif\"><span style=\"font-size: 13.3333px;\"><b>Patra (109 Riga Fereou str.)</b></span></font></p><p class=\"MsoNormal\" align=\"center\" style=\"text-align:center\"><font face=\"Segoe UI Historic, sans-serif\"><span style=\"font-size: 13.3333px;\"><b>Larisa (66 Kiprou str.)</b></span></font></p><p class=\"MsoNormal\" align=\"center\" style=\"text-align:center\"><font face=\"Segoe UI Historic, sans-serif\"><span style=\"font-size: 13.3333px;\"><b>Agrinio (7 Mpoukogianni str.)</b></span></font></p><p class=\"MsoNormal\" align=\"center\" style=\"text-align:center\"><font face=\"Segoe UI Historic, sans-serif\"><b><span lang=\"EN-US\" style=\"line-height: 115%; background: black; font-size: 13.3333px;\"></span></b></font></p><p class=\"MsoNormal\" align=\"center\" style=\"text-align:center\"><font face=\"Segoe UI Historic, sans-serif\"><span style=\"font-size: 13.3333px;\"><b>Volos (37-39 K. Kartali str.)</b></span></font></p><p class=\"MsoNormal\" align=\"center\" style=\"text-align:center\"><br></p>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
